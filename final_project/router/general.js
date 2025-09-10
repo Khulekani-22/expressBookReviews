ea683e3642aef28af367f74869317b5d3754d687
Vikash Sharma <vks23192@gmail.com>
2025-09-10 11:17:49 -0400

    Task 1 to 3 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,21 +12,42 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const bookList = JSON.stringify(books, null, 4); // 4-space indentation for readability
+     return res.status(200).send(bookList);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+
+  // Check if the book exists
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn]);
+  } else {
+    return res.status(404).json({ message: "Book not found for the given ISBN." });
+  }
+
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+    const authorName = req.params.author;
+    const matchingBooks = [];
+  
+    // Iterate through all books and find matches
+    for (let key in books) {
+      if (books[key].author.toLowerCase() === authorName.toLowerCase()) {
+        matchingBooks.push({ isbn: key, ...books[key] });
+      }
+    }
+  
+    if (matchingBooks.length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res.status(404).json({ message: "No books found for the given author" });
+    }
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
