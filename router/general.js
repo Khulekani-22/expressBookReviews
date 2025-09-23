61c3596d0a5759d233e0b51eae749889d3fc012a
Supriya Eshwar <supriyaeshwar@Supriyas-MacBook-Pro.local>
2025-09-23 18:06:22 +0200

    Task 1 to 7

---
+++ router/general.js
@@ -0,0 +1,80 @@
+const express = require("express");
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+const doesExist = (username) => {
+  let userswithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  return userswithsamename.length > 0;
+};
+
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user." });
+});
+
+// Get the book list available in the shop
+public_users.get("/", function (req, res) {
+  //Write your code here
+  return res.send(JSON.stringify({ books }, null, 4));
+});
+
+// Get book details based on ISBN
+public_users.get("/isbn/:isbn", function (req, res) {
+  let { isbn } = req.params;
+  //   return res.send('hello');
+
+  return res.send(books[isbn]);
+});
+
+// Get book details based on author
+public_users.get("/author/:author", function (req, res) {
+  let { author } = req.params;
+
+  author = author.split("_").join("").toLowerCase();
+
+  let wantedBook = Object.keys(books).filter((book) => {
+    return books[book].author.split(" ").join("").toLowerCase() === author;
+  });
+  wantedBook.length
+    ? res.send(books[wantedBook])
+    : res.send("Couldn't find the book");
+});
+
+// Get all books based on title
+public_users.get("/title/:title", function (req, res) {
+  let { title } = req.params;
+
+  title = title.split("_").join("").toLowerCase();
+
+  let wantedBook = Object.keys(books).filter((book) => {
+    return books[book].title.split(" ").join("").toLowerCase() === title;
+  });
+  wantedBook.length
+    ? res.send(books[wantedBook])
+    : res.send("Couldn't find the book");
+});
+
+//  Get book review
+public_users.get("/review/:isbn", function (req, res) {
+  let { isbn } = req.params;
+
+  return res.send(books[isbn].reviews);
+});
+
+module.exports.general = public_users;
