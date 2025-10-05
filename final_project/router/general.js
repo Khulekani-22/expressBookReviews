53fe74c4e9a295297a33cfa85b6db2cbb01aad8e
Clement-NI <niyuzhen2020@gmail.com>
2025-10-05 15:37:40 +0200

    Task 1
    Get all books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,6 +13,9 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+  if(books){
+    return res.status(200).send(JSON.stringify(books));
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
